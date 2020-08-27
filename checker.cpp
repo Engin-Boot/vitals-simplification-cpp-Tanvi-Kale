fe74620c1f7c3ab7df2a7e7d1cf884c1a51690cb
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-08-27 11:02:48 +0530

    update

--- checker.cpp
+++ checker.cpp
@@ -24,18 +24,10 @@ bool vitals_respRate(float respRate)
   return true;
 }
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
-}
-
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitals_bpm(80) == true);
+  assert(vitals_spo2(95) == true);
+  assert(vitals_respRate(60) == true);
+  
+    return 0;
 }
