cd4914a2693f4b803bc8e9cc9b9c27f333602d17
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-08-27 10:56:31 +0530

    update

--- checker.cpp
+++ checker.cpp
@@ -1,5 +1,29 @@
 #include <assert.h>
 
+bool vitals_bpm(float bpm)
+{
+    if(bpm < 70 || bpm > 150) {
+    return false;
+    }
+  return true;
+}
+  
+bool vitals_spo2(float spo2)
+{
+  if(spo2 < 90) {
+  return false;
+  }
+  return true;
+}
+  
+bool vitals_respRate
+{
+  if(respRate < 30 || respRate > 95) {
+  return false;
+  }
+  return true;
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
   if(bpm < 70 || bpm > 150) {
     return false;
