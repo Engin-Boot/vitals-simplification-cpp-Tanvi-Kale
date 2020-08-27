67b5cdce6696caed510efcd1e82f619dadfd08e3
Tanvi-Kale <69624205+Tanvi-Kale@users.noreply.github.com>
2020-08-27 12:24:08 +0530

    update

--- checker.cpp
+++ checker.cpp
@@ -7,15 +7,16 @@ bool vitalsRangeIsOk(float value,int lowerLimit,int upperLimit)
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
     
-    assert(vitalsRangeIsOk(160,70,150) == false);
-    assert(vitalsRangeIsOk(20,70,150) == false);
-    assert(vitalsRangeIsOk(70,70,150) == true);
-    assert(vitalsRangeIsOk(90,70,150) == true);
     return (vitalsRangeIsOk(bpm,70,150) && vitalsRangeIsOk(spo2,90,100) && vitalsRangeIsOk(respRate,30,95));
 }
         
 int main() {
 
+  assert(vitalsRangeIsOk(160,70,150) == false);
+  assert(vitalsRangeIsOk(20,70,150) == false);
+  assert(vitalsRangeIsOk(70,70,150) == true);
+  assert(vitalsRangeIsOk(90,70,150) == true);  
+    
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
   return 0;
